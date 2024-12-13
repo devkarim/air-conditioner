d94b578abb2ef11914c19490dffc41d881f08375
Karim Wael <devkarim@hotmail.com>
2024-12-13 16:23:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Air Conditioner - TM4C123GH6PM
+
+## Overview
+
+An air conditioning system built using the TM4C123GH6PM microcontroller. The project simulates an air conditioner with heating and cooling modes based on the ambient temperature compared to a target temperature.
+
+## Features
+
+1. Temperature Sensor: Reads ambient temperature using the LM35 sensor (analog input).
+
+2. LED Indicators:
+   Blue LED: Indicates cooling mode (ambient temperature < target temperature).
+   Red LED: Indicates heating mode (ambient temperature > target temperature).
+
+3. Target Temperature Adjustment:
+   Two buttons to edit the target temperature.
+
+4. Display Output:
+   The Nokia 5110 display shows:
+   Current system status: "Heater" or "Cooler"
+   Target temperature.
+
+5. UART Communication:
+   Ambient temperature is periodically sent to a computer for real-time monitoring.
+
+6. Timer-Based Check:
+   If ambient and target temperatures are equal, the system waits for a few seconds (using a timer interrupt) before rechecking.
+
+## Components
+
+### Hardware
+
+- Microcontroller: TM4C123GH6PM.
+- Temperature Sensor: LM35 (analog output).
+- LEDs: Blue and red for displaying the current mode.
+- Buttons: Two push buttons for editing the target temperature.
+- Display: Nokia 5110 LCD for showing the current mode and target temperature.
+- UART: Interface for transmitting ambient temperature to a computer.
+
+### Software
+
+- IDE: Keil Microvision.
+- Programming Language: C.
---
+++ SpaceInvaders.c
@@ -0,0 +1,68 @@
+// Red SparkFun Nokia 5110 (LCD-10168)
+// -----------------------------------
+// Signal        (Nokia 5110) LaunchPad pin
+// 3.3V          (VCC, pin 1) power
+// Ground        (GND, pin 2) ground
+// SSI0Fss       (SCE, pin 3) connected to PA3
+// Reset         (RST, pin 4) connected to PA7
+// Data/Command  (D/C, pin 5) connected to PA6
+// SSI0Tx        (DN,  pin 6) connected to PA5
+// SSI0Clk       (SCLK, pin 7) connected to PA2
+// back light    (LED, pin 8) not connected, consists of 4 white LEDs which draw ~80mA total
+
+#include "./tm4c123gh6pm.h"
+#include "Nokia5110.h"
+#include "Random.h"
+#include "TExaS.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Timer2_Init(unsigned long period);
+void Delay100ms(unsigned long count); // time delay in 0.1 seconds
+unsigned long TimerCount;
+unsigned long Semaphore;
+
+
+int main(void){
+  TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
+  Random_Init(1);
+  Nokia5110_Init();
+  Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();      // draw buffer
+
+
+  //Nokia5110_Clear();
+  //Nokia5110_SetCursor(2, 4);
+  //Nokia5110_OutUDec(1234);
+  while(1){
+  }
+
+}
+
+
+// You can use this timer only if you learn how it works
+void Timer2_Init(unsigned long period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  TimerCount = 0;
+  Semaphore = 0;
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+
+void Timer2A_Handler(void){ 
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+  TimerCount++;
+  Semaphore = 1; // trigger
+}
