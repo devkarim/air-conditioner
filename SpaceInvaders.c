874070fff09cd6b1334ae1c3c4937b75e81c0cd7
Karim Wael <devkarim@hotmail.com>
2024-12-22 20:07:10 +0200

    feat: remove unnecessary comments

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -80,39 +80,9 @@ int main(void){
   Nokia5110_ClearBuffer();
 	Nokia5110_DisplayBuffer();      // draw buffer
 
-
-  //Nokia5110_Clear();
-  //Nokia5110_SetCursor(2, 4);
-  //Nokia5110_OutUDec(1234);
   while(1){
   }
 
-}
-
-
-// You can use this timer only if you learn how it works
-void Timer2_Init(unsigned long period){ 
-  unsigned long volatile delay;
-  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
-  delay = SYSCTL_RCGCTIMER_R;
-  timerCount = 0;
-  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
-  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
-  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
-  TIMER2_TAILR_R = period-1;    // 4) reload value
-  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
-  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
-  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
-  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
-// interrupts enabled in the main program after all devices initialized
-// vector number 39, interrupt number 23
-  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
-  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
-}
-
-void Timer2A_Handler(void){ 
-  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
-  timerCount++;
 }
 
 void updateDisplay(void) {
